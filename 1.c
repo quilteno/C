d6254aee19fde13bcfb8ee78eea37f571567198f
quilteno <14162564437@qq.com>
2020-12-20 18:51:34 +0800

    随便改了几行

--- 1.c
+++ 1.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
 /*头文件*/
-void f(int *p); //定义函数
+void f(int *p); 
+//定义函数
 int main()
 {
     int a = 4;      
     a++;
     f(&a);
+    a--;
     a++;
-    f           
-    a++;
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            for (int k = 0; k < 10; k++)
-            {
-                for (int l = 0; l < count; l++)
-                {
-                    
-                    /* code */
-                }
-                
-                a++;
-            }
-        }
-    }
     return 0;
 }
 
 void f(int *p)
 {
-
     printf("a=%d\n", *p);
 }
