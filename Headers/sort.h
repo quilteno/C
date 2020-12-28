21c6c03b6018f0b79e58f44a8a6a41c2b03556e7
quilteno <1416256437@qq.com>
2020-12-28 09:52:16 +0800

    移植了堆排序到头文件HeapSort

--- Headers/sort.h
+++ Headers/sort.h
@@ -8,6 +8,8 @@ int bubbleSort(int *bubble_a, int n);
 void choiceSort(int *choice_a, int n);
 void merge(int arr[], int start, int mid, int end);
 void mergeSort(int arr[], int start, int end);
+void HeapSort(int a[], int n);
+void HeapAdjust(int a[], int s, int m);
 
 //冒泡排序
 int bubbleSort(int *bubble_a, int bubble_n)
@@ -106,5 +108,32 @@ void mergeSort(int arr[], int start, int end)
     mergeSort(arr, mid + 1, end);
     merge(arr, start, mid, end);
 }
-
+//堆排序
+void HeapAdjust(int a[],int s,int m)//一次筛选的过程
+{
+    int rc,j;
+    rc=a[s];
+    for(j=2*s;j<=m;j=j*2)//通过循环沿较大的孩子结点向下筛选
+    {
+        if(j<m&&a[j]<a[j+1]) j++;//j为较大的记录的下标
+        if(rc>a[j]) break;
+        a[s]=a[j];s=j;
+    }
+    a[s]=rc;//插入
+}
+void HeapSort(int a[],int n)
+{
+    int temp,i;
+    for(i=n/2;i>0;i--)//通过循环初始化顶堆
+    {
+        HeapAdjust(a,i,n);
+    }
+    for(i=n;i>0;i--)
+    {
+        temp=a[1];
+        a[1]=a[i];
+        a[i]=temp;//将堆顶记录与未排序的最后一个记录交换
+        HeapAdjust(a,1,i-1);//重新调整为顶堆
+    }
+}
 #endif
--- Sources/sort.c
+++ Sources/sort.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "sort.h"
 // 头文
-#define N 500000
-#define L 2000
-void arrprint(int number[]);
-void randomNumber(int numebr[], int len);
+#define N 10000//生成数组大小
+#define L 20//共输出多少行，注释掉arrPrint可不管
+void arrprint(int number[]);//输出数组函数
+void randomNumber(int numebr[], int len);//生成随机数
 
 int main(int argc, char *argv[])
 {
@@ -16,11 +17,13 @@ int main(int argc, char *argv[])
     //设置样本
     clock_t start = clock();//记录时间
     ///////////////排序函数放置位置///////////////
+    HeapSort(a, N);
     // mergeSort(a,0,N-1);
     // bubbleSort(a, N);
-    choiceSort(a,N);
+    // choiceSort(a,N);
     ////////////////////////////////////////////
     printf("Run Time:%f\n", ((double)clock() - start) / CLOCKS_PER_SEC);//打印程序运行花费时间
+    printf("midum=%d\nlast=%d\n", a[N / 2], a[N-1]);//打印中值和尾值
     // arrPrint(a, N);
     return 0;
 }
@@ -29,13 +32,13 @@ void arrPrint(int *number, int len)
 {
     printf("整理后为\n");
     int flag = 0;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len; i+=N/100)
     {
         printf("%03d ", *(number + i));
         flag++;
         if (flag % (N / L) == 0)
         {
-            printf("\n");
+            printf("\n");//打印L行
         }
     }
 }
@@ -43,6 +46,6 @@ void randomNumber(int *number, int len)
 {
     for (int i = 0; i < len; i++)
     {
-        number[i] = rand() % N;
+        number[i] = rand() % 1000;//生成最大1000的随机数 
     }
 }
